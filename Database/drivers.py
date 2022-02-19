9c7f767494baf02834417fdd1ef1de4b355422bd
pavobravo11 <gustavoa1011@gmail.com>
2022-02-19 16:57:12 -0700

    Update drivers.py

--- Database/drivers.py
+++ Database/drivers.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 employees_df = pd.read_csv("employees.csv")
 
+drivers = employees_df[employees_df["Carpool_Driver"] == 1]
+
 names_address_dict = dict(zip(
     employees_df.First_Name + " " + employees_df.Last_Name, 
     employees_df.Address))
+
+print(drivers)
+print(names_address_dict)
