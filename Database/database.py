11e8c2d8758f4fecc8f49a18c47a3a64d62bb7a3
vanessahychen <60018155+vanessahychen@users.noreply.github.com>
2022-02-19 15:15:54 -0700

    Database Read

--- Database/database.py
+++ Database/database.py
@@ -1,1 +1,19 @@
-#test
+import mysql.connector
+import pandas as pd;
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="vanessa",
+  password="vanessa",
+  database='car_pool_planner'
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM employee")
+
+myresult = mycursor.fetchall()
+
+employee_df = pd.read_sql("SELECT * FROM employee", con=mydb, index_col="Employee_ID")
+
+print(employee_df)
