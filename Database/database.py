5f8c8849bdc1f8583a9abe60e81d28f9333fc7ab
vanessahychen <60018155+vanessahychen@users.noreply.github.com>
2022-02-19 17:48:27 -0700

    Added more tables

--- Database/database.py
+++ Database/database.py
@@ -15,7 +15,14 @@ mycursor.execute("SELECT * FROM employee")
 myresult = mycursor.fetchall()
 
 employee_df = pd.read_sql("SELECT * FROM employee", con=mydb, index_col="Employee_ID")
+carpool_groups_df = pd.read_sql("SELECT * FROM carpool_groups", con=mydb, index_col="fk_Employee_ID")
+driver_df = pd.read_sql("SELECT * FROM driver", con=mydb, index_col="Driver_ID")
+company_df = pd.read_sql("SELECT * FROM company", con=mydb, index_col="Company_ID")
+
 
 print(employee_df)
 
 employee_df.to_csv("employees.csv")
+carpool_groups_df.to_csv("carpool_groups.csv")
+driver_df.to_csv("driver.csv")
+company_df.to_csv("company.csv")
