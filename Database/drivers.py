565a7e764cc75268c238ebeec3b75b4506aa956c
pavobravo11 <gustavoa1011@gmail.com>
2022-02-19 17:03:58 -0700

    Added dataframe containing those able to drive

--- Database/drivers.py
+++ Database/drivers.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 employees_df = pd.read_csv("employees.csv")
 
-drivers = employees_df[employees_df["Carpool_Driver"] == 1]
+# Gets names and employee ID's of those able to drive
+drivers = employees_df[employees_df["Carpool_Driver"] == 1].drop(["Address", "City","Carpool_Driver"], axis=1).set_index("Employee_ID")
 
+# Gets all the names and addresses of employees
 names_address_dict = dict(zip(
     employees_df.First_Name + " " + employees_df.Last_Name, 
     employees_df.Address))
 
-print(drivers)
-print(names_address_dict)
