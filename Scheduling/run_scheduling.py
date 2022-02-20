92d7ac484daf2f2bb63e75410a75d31e3d85f9a2
matthew-mcc <matthew.mcconnell1@ucalgary.ca>
2022-02-19 20:25:18 -0700

    Big merge

--- Scheduling/run_scheduling.py
+++ Scheduling/run_scheduling.py
@@ -1,24 +1,9 @@
 from schedulingFinal import get_ride_order
-
-import pandas as pd
-
-employees_df = pd.read_csv(R"Scheduling\employees.csv", index_col="Employee_ID")
-group_df = pd.read_csv(R"Scheduling\carpool_groups.csv", index_col="fk_Employee_ID")
-drivers_df = pd.read_csv(R"Scheduling\driver.csv", index_col="Driver_ID")
-
-group_drivers_df = employees_df[group_df["Group_Driver"] == 1].join(drivers_df, on="Driver_ID")
-
-group_drivers_dict = {}
-for idx, item in group_drivers_df.iterrows():
-    address_and_max_seats = (item["Address"] + ", " + item["City"], item["Max_Seats"])
-    full_name = item["First_Name"] + "_" + item["Last_Name"]
-    group_drivers_dict[full_name] = address_and_max_seats
+from Database.database import database_to_dic
 
 
-non_drivers_df = employees_df[group_df["Group_Driver"] == 0]
-non_drivers_dict = dict(zip(
-    non_drivers_df.First_Name + "_" + non_drivers_df.Last_Name,
-    non_drivers_df.Address + ", " + non_drivers_df.City))
+
+group_drivers_dict, non_drivers_dict = database_to_dic()
 
 wp = {
     "UniversityOfCalgary": "2500 University Dr NW, Calgary"}
