391c1be5bf578d19f53504ca7c64fc2a9bbba59c
vanessahychen <60018155+vanessahychen@users.noreply.github.com>
2022-02-19 20:00:43 -0700

    Not done adding insert

--- Database/database.py
+++ Database/database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import pandas as pd
+from mysql.connector import Error
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -26,3 +27,35 @@ employee_df.to_csv("employees.csv")
 carpool_groups_df.to_csv("carpool_groups.csv")
 driver_df.to_csv("driver.csv")
 company_df.to_csv("company.csv")
+
+
+def insert_employee_db(id, fname, lname, address, city, password):
+    query = "INSERT INTO employee(Employee_ID, First_Name, Last_Name, Address, City, Cannot_Drive, Password, " \
+            "Admin, Driver_ID, Passenger_Rides, Driver_Rides, Rank) " \
+            "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    args = (id, fname, lname, address, city, 0, password, 0, 8, 0, 0, 0)
+    print("args: ", args)
+    mycursor = mydb.cursor()
+    mycursor.execute(query, args)
+    mydb.commit()
+    # try:
+    #     # db_config = read_db_config()
+    #     # conn = MySQLConnection(**db_config)
+
+    #     mycursor = mydb.cursor()
+    #     mycursor.execute(query, args)
+
+    #     if mycursor.lastrowid:
+    #         print('last insert id', mycursor.lastrowid)
+    #     else:
+    #         print('last insert id not found')
+
+    #     mydb.commit()
+    # except Error as error:
+    #     print(error)
+
+    # finally:
+    #     mycursor.close()
+    #     mydb.close()
+
+insert_employee_db(10, "Max", "Brown", "131 Christie Knoll Point SW", "Calgary", "password10")
