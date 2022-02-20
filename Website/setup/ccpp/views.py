5e0f18f40888456daa0d638bed470743e37ed490
pavobravo11 <gustavoa1011@gmail.com>
2022-02-20 05:50:09 -0700

    Update views.py

--- Website/setup/ccpp/views.py
+++ Website/setup/ccpp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
+from Scheduling.schedulingFinal import get_ride_order
+from Database.database import database_to_dict, update_group_id, change_drivers
 
 def geeks_view(request):
     # create a dictionary
@@ -9,6 +11,31 @@ def geeks_view(request):
     }
     # return response
     return render(request, "home.html", context)
+
+def refresh_schedule(request):
+    # Change the drivers right before alg is ran
+    # change_drivers()
+
+    # # retrieve drivers and non drivers
+    # group_drivers_dict, non_drivers_dict = database_to_dict()
+
+    # wp = {
+    #     "UniversityOfCalgary": "2500 University Dr NW, Calgary"}
+
+    # # run algo
+    # group_list, location_data = get_ride_order(group_drivers_dict, non_drivers_dict, wp)
+
+    # # Update the database
+    # count = 0
+    # for k, v in group_list.items():
+    #     for i in v:
+    #         update_group_id(i, count)
+    #     update_group_id(k, count)
+    #     count += 1 
+    print("Refresh is working")
+
+    return render(request, "home.html")
+
 
 def test1(request):
     context = {
