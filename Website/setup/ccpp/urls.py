8453abb46128dd294be2a041e6a18d10a9dfadb4
Kevin Van <vankevin58@gmail.com>
2022-02-19 17:31:58 -0700

    Login page
    Made login page without a logout option

--- Website/setup/ccpp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, re_path                                                                                                                        
-from . import views
-
-urlpatterns = [ 
-    re_path(r'', views.default_map, name="default"),
-]
--- Website/setup/setup/urls.py
+++ Website/setup/setup/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
-    path('', include('ccpp.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), # new
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
