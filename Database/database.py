235b46ea194dca6796e7b789939d450eafe4d9be
pavobravo11 <gustavoa1011@gmail.com>
2022-02-19 21:42:17 -0700

    Changed the dictionary to return employee ID instead of name

--- Database/database.py
+++ Database/database.py
@@ -22,6 +22,11 @@ def database_to_dic():
   driver_df = pd.read_sql("SELECT * FROM driver", con=mydb, index_col="Driver_ID")
   company_df = pd.read_sql("SELECT * FROM company", con=mydb, index_col="Company_ID")
 
+  # employee_df.to_csv("employees.csv")
+  # carpool_groups_df.to_csv("carpool_groups.csv")
+  # driver_df.to_csv("driver.csv")
+  # company_df.to_csv("company.csv")
+
   # Get dictionary of those chosen to drive the groups
   group_drivers_df = employee_df[carpool_groups_df["Group_Driver"] == 1].join(driver_df, on="Driver_ID")
   print(group_drivers_df.head())
@@ -29,19 +34,16 @@ def database_to_dic():
 
   for idx, item in group_drivers_df.iterrows():
       address_and_max_seats = (item["Address"] + ", " + item["City"], item["Max_Seats"])
-      full_name = item["First_Name"] + "_" + item["Last_Name"]
-      group_drivers_dict[full_name] = address_and_max_seats
+      group_drivers_dict[item["Employee_ID"]] = address_and_max_seats
 
   # Get dictionary of those who were not chosen to drive
   non_drivers_df = employee_df[carpool_groups_df["Group_Driver"] == 0]
   non_drivers_dict = dict(zip(
-      non_drivers_df.First_Name + "_" + non_drivers_df.Last_Name,
+      # non_drivers_df.First_Name + "_" + non_drivers_df.Last_Name,
+      non_drivers_df.Employee_ID,
       non_drivers_df.Address + ", " + non_drivers_df.City))
   return group_drivers_dict, non_drivers_dict
-# employee_df.to_csv("employees.csv")
-# carpool_groups_df.to_csv("carpool_groups.csv")
-# driver_df.to_csv("driver.csv")
-# company_df.to_csv("company.csv")
+
 
 
 def insert_employee_db(id, fname, lname, address, city, password):
@@ -96,4 +98,4 @@ def insert_employee_db(id, fname, lname, address, city, password):
 #     #     mycursor.close()
 #     #     mydb.close()
 
-insert_employee_db(10, "Max", "Brown", "131 Christie Knoll Point SW", "Calgary", "password10")
+# insert_employee_db(10, "Max", "Brown", "131 Christie Knoll Point SW", "Calgary", "password10")
