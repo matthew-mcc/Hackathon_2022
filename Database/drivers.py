7d21ad39a7bc1db17a09ded2a42fcd1c2637654b
pavobravo11 <gustavoa1011@gmail.com>
2022-02-19 18:45:22 -0700

    Created dictionaries of drivers and non-drivers

--- Database/drivers.py
+++ Database/drivers.py
@@ -5,11 +5,16 @@ group_df = pd.read_csv("carpool_groups.csv", index_col="fk_Employee_ID")
 drivers_df = pd.read_csv("driver.csv", index_col="Driver_ID")
 
 group_drivers_df = employees_df[group_df["Group_Driver"] == 1].join(drivers_df, on="Driver_ID")
-group_drivers_df
 
-print(group_drivers_df)
+group_drivers_dict = {}
+for idx, item in group_drivers_df.iterrows():
+    address_and_max_seats = (item["Address"] + ", " + item["City"], item["Max_Seats"])
+    full_name = item["First_Name"] + "_" + item["Last_Name"]
+    group_drivers_dict[full_name] = address_and_max_seats
 
-# group_drivers_dict = dict(zip(
-#     group_drivers_df.First_Name + "_" + group_drivers_df.Last_Name,
 
-# ))
+non_drivers_df = employees_df[group_df["Group_Driver"] == 0]
+non_drivers_dict = dict(zip(
+    non_drivers_df.First_Name + "_" + non_drivers_df.Last_Name,
+    non_drivers_df.Address + ", " + non_drivers_df.City))
+
