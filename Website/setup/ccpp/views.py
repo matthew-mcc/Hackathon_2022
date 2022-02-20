4c67c5d2c67a1ca1a2880283192aaca30cc51880
Kevin Van <vankevin58@gmail.com>
2022-02-20 03:52:06 -0700

    worky varaible into html

--- Website/setup/ccpp/views.py
+++ Website/setup/ccpp/views.py
@@ -2,14 +2,14 @@ from pickle import NONE
 from django.shortcuts import render
 from django.template import loader
 
-# Create your views here.
-def default_map(request):
-    # TODO: move this token to Django settings from an environment variable
-    # found in the Mapbox account settings and getting started instructions
-    # see https://www.mapbox.com/account/ under the "Access tokens" section
-    mapbox_access_token = 'pk.my_mapbox_access_token'
-    return render(request, 'default.html', 
-                  { 'mapbox_access_token': mapbox_access_token })
+def geeks_view(request):
+    # create a dictionary
+    context = {
+        "first_name" : "Naveen",
+        "last_name"  : "Arora",
+    }
+    # return response
+    return render(request, "home.html", context)
 
 
 def send_data(request):
--- Website/setup/setup/settings.py
+++ Website/setup/setup/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'setup.urls'
+ROOT_URLCONF = 'ccpp.urls'
 
 TEMPLATES = [
     {
