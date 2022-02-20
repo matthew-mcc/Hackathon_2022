855844e2817f0d30bf1e16464e919a9f1dfdf378
matthew-mcc <matthew.mcconnell1@ucalgary.ca>
2022-02-20 06:57:15 -0700

    ok

--- Website/setup/ccpp/urls.py
+++ Website/setup/ccpp/urls.py
@@ -17,13 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.views.generic.base import TemplateView # new
-from .views import geeks_view
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), # new
-    path('', geeks_view),
-    
+    path('', views.geeks_view),
     path('', TemplateView.as_view(template_name='home.html'), name='home',),
 ]
